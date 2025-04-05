7918f387ba51c49fa59558e676d75de9190fa89e
viniciusveu <viniciusvedovotto@gmail.com>
2025-04-05 15:49:13 -0300

    chore: update event emiter url path

--- apps/event-worker/src/event-worker.controller.ts
+++ apps/event-worker/src/event-worker.controller.ts
@@ -3,11 +3,11 @@ import { EventWorkerService } from './event-worker.service';
 import { EventDataDto } from '@app/shared/dtos';
 import { JwtAuthGuard } from '@app/auth';
 
-@Controller()
+@Controller('events')
 export class EventWorkerController {
   constructor(private readonly eventWorkerService: EventWorkerService) {}
 
-  @Post()
+  @Post('emit')
   @UseGuards(JwtAuthGuard)
   emitEvent(@Body() data: EventDataDto): void {
     this.eventWorkerService.emitEvent(data);
